028d292ac0665c1dd27f795a6e0337c771430aa4
eankeen <24364012+eankeen@users.noreply.github.com>
2020-04-19 14:49:35 -0700

    dw: enable linting of tsx files

--- src/ui/popup.tsx
+++ src/ui/popup.tsx
@@ -1,10 +1,10 @@
+import '../styles/popup.css'
+
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 
-import '../styles/popup.css'
-
 class Hello extends React.Component {
-  render() {
+  render(): JSX.Element {
     return (
       <div className="popup-padded">
         <h1>{chrome.i18n.getMessage('l10nHello')}</h1>
