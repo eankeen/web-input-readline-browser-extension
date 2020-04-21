db271f8f42fecaae9c9d6101b8424f1ab8b0912a
eankeen <24364012+eankeen@users.noreply.github.com>
2020-04-21 00:08:32 -0700

    fix: create prepublishing script
    this bundles for production and runs gulp task that creates zip files,
    ready to be uploaded to chrome web store

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+import { dest, src } from 'gulp'
+import plumber from 'gulp-plumber'
+import zip from 'gulp-zip'
+import del from 'del'
+
+export async function buildPackages() {
+  const chromeZip = 'chrome.zip'
+  const firefoxZip = 'firefox.zip'
+
+  del.sync([
+    `dist/${chromeZip}`,
+    `dist/${firefoxZip}`
+  ])
+
+  src('dist/chrome')
+    |> plumber()
+    |> zip(chromeZip)
+    |> dest('dist')
+
+  src('dist/firefox')
+    |> plumber()
+    |> zip(firefoxZip)
+    |> dest('dist')
+}
