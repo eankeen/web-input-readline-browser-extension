af1e11866a93142a1865820e44d107ec85fbc7d2
eankeen <24364012+eankeen@users.noreply.github.com>
2020-04-21 19:55:17 -0700

    docs: create simple website showing readme

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,11 @@
+// @ts-nocheck
+import through from 'through2'
+import htmlMinify from 'gulp-htmlmin'
 import { dest, src } from 'gulp'
 import plumber from 'gulp-plumber'
+import markdown from 'gulp-markdown'
 import zip from 'gulp-zip'
+import rename from 'gulp-rename'
 import del from 'del'
 
 export async function buildPackages() {
@@ -24,4 +29,41 @@ export async function buildPackages() {
     |> plumber()
     |> zip(sourceZip)
     |> dest('dist')
+}
+
+export async function docs() {
+  const markdownFile = 'readme.md'
+
+  src(markdownFile)
+    |> plumber()
+    |> markdown()
+    |> rename('index.html')
+    |> addTags()
+    |> htmlMinify({ collapseWhitespace: true })
+    |> dest('docs')
+}
+
+function addTags() {
+  return through.obj((file, enc, cb) => {
+    let siteHtml = String(file.contents);
+
+    const newSiteHtml = `
+      <!DOCTYPE html> \
+      <html> \
+        <head> \
+          <link rel="preconnect" href="https://cdn.jsdelivr.net" crossorigin> \
+          <link rel="dns-prefetch" href="https://cdn.jsdelivr.net"> \
+          <link \
+            rel="stylesheet" \
+            href="https://cdn.jsdelivr.net/npm/fox-css/dist/fox.dark.min.css" \
+          > \
+        </head> \
+        <body> \
+          ${siteHtml} \
+        </body> \
+      </html>`
+
+    file.contents = new Buffer.from(newSiteHtml);
+    return cb(null, file);
+  });
 }
